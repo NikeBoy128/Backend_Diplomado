70a1361a60c9e9b07ad9e6272b1b092e79dd75b4
Harold Mosquera <haroldmosquera739@gmail.com>
2023-11-03 16:07:19 -0500

    add documentation api

--- jjplus/settings.py
+++ jjplus/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,23 @@
 from rest_framework import routers
 from .api import projectViewSet
-router=routers.DefaultRouter()
-router.register('api/projects',projectViewSet,'projects')
-urlpatterns=router.urls
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path, re_path
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your Project API",
+      default_version='v1',
+      description="API documentation for Your Project",
+   ),
+   public=True,
+)
+
+router = routers.DefaultRouter()
+router.register('api/projects', projectViewSet, 'projects')
+
+urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] + router.urls
