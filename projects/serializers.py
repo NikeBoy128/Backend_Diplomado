ee863669fedfdd78d613a071eeb38daedadeb49a
Harold Mosquera <haroldmosquera739@gmail.com>
2023-11-06 09:02:02 -0500

    update password

--- projects/serializers.py
+++ projects/serializers.py
@@ -5,3 +5,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name','password','is_staff','is_active')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # Hash the password
+        user.save()
+        return user   
--- projects/views.py
+++ projects/views.py
@@ -11,8 +11,8 @@ from rest_framework.decorators import authentication_classes, permission_classes
 class ExampleView(APIView):
     def get(self, request, format=None):
         content = {
-            'user': str(request.user),  # `django.contrib.auth.User` instance.
-            'auth': str(request.auth),  # None
+            'user': str(request.user),  
+            'auth': str(request.auth),  
         }
         return Response(content)
 
