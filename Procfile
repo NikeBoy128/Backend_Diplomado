e0513eeb674737485e37aad624c15554a2cbeefb
Harold Mosquera <haroldmosquera739@gmail.com>
2023-11-11 14:26:10 -0500

    heroku update

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jjplus.wsgi --log-file -
--- projects/urls.py
+++ projects/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + router.urls
+] + router.urls +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
