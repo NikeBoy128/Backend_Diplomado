1e92631b8503b5683794e1f850e29cd62ac297ad
Harold Mosquera <haroldmosquera739@gmail.com>
2023-11-11 14:35:03 -0500

    allowed hosts heroku

--- jjplus/settings.py
+++ jjplus/settings.py
@@ -26,11 +26,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY',default='your_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG='RENDER' not in os.environ
-ALLOWED_HOSTS = []
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+ALLOWED_HOSTS = ['*']
+
 # Application definition
 
 INSTALLED_APPS = [
