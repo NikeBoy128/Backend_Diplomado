655fd6b133f44589e8a34edde3c984f2f7f046aa
Harold Mosquera <haroldmosquera739@gmail.com>
2023-11-18 14:58:39 -0500

    corsheaders update

--- jjplus/settings.py
+++ jjplus/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -55,8 +56,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
-
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
